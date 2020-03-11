e5c04f98fae7e7a2b13399ada8800c1ef6faacb6
Arthur Coruble <arthur.coruble@dashlane.com>
2020-03-11 15:04:42 +0100

    Add BlueprintJS components and start Dark / light theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
+
+import React from "react";
+import { Button, Classes } from "@blueprintjs/core";
 
 function App() {
+  document.body.className = Classes.DARK; // Used to set Dark / light theme on all components at once
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="test" />
     </div>
   );
 }
